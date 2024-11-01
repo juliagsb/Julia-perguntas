ab0ab3857b2ec614058da89098e1cf4d95bf4499
juliagsb <163548705+juliagsb@users.noreply.github.com>
2024-11-01 10:44:50 -0300

    Add files via upload

---
+++ Julia/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Em qual período da pré-história o fogo foi descoberto?',
+    'A Era do Paleolítico'
+)
+
+criaCartao(
+    'Geografia',
+    'O que sinifica o nome do estado do Paraná?',
+    'Tão grande quanto o mar'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos litros de sangue uma pessoa tem? ',
+    'De 2 a 4 litros'
+)
+
+criaCartao(
+    'Lingua espanhol',
+    'Como se diz oi em espanhol?',
+    'Oi em espanhol é hola'
+)
